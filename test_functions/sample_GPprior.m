6c8885167a573d6570efa7bc0fca34fe2810917a
ziw <ziw@mit.edu>
2017-03-20 13:16:33 -0400

    update

---
+++ test_functions/sample_GPprior.m
@@ -0,0 +1,9 @@
+function f = sample_GPprior(dx)
+nFeatures = 10000;
+l = ones(1, dx)*256.;
+sigma = 5;
+sigma0 = 0.0001;
+W = randn(nFeatures, dx) .* repmat(sqrt(l), nFeatures, 1);
+b = 2 * pi * rand(nFeatures, 1);
+theta = randn(nFeatures, 1);
+f = @(x) (theta' * sqrt(2 * sigma / nFeatures) * cos(W * x' + repmat(b, 1, size(x, 1))))';
